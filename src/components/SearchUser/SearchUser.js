b3b2a4c175105f81d15e7890ec349443f937706c
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-02-18 12:34:03 -0500

    working

--- src/components/SearchUser/SearchUser.js
+++ src/components/SearchUser/SearchUser.js
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
+import config from '../../config'
 import "./SearchUser.css";
 
 class SearchUser extends Component {
   handleSearch = e => {
     e.preventDefault();
-    console.log(e.target.value);
+    const {user_search} = e.target
+    console.log(user_search.value);
+
+    fetch(`${config.API_ENDPOINT}/user/${user_search.value}`,{
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+  })
+  .then(res => {
+      if(!res.ok){
+          throw new Error(res.status)
+      }
+      return res.json()
+  })
+  .then(data => console.log(data))
+
+    
   };
 
   render() {
     return (
-      <>
+     
+      <form onSubmit={this.handleSearch}>
+        <label htmlFor="user_search">Frenmo Search</label>
         <input
+          id="user_search"
           type="text"
-          name="user-search"
-          placeholder="Search friends"
-          onChange={e => this.handleSearch(e)}
+          name="user_search"
+          placeholder="find friends"
         />
-      </>
+        <button type="submit">Add</button>
+      
+      </form>
     );
   }
 }
