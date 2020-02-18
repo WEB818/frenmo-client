9b7d1945a11b16639e12d92d259f18be6be13614
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-02-17 22:44:56 -0800

    adding redeem/favor_id

--- src/components/Header/NavMenu.js
+++ src/components/Header/NavMenu.js
@@ -5,7 +5,7 @@ import TokenService from "../../services/token-service";
 // import Icon from "../../images/profile.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-import { withRouter } from "react-router-dom";
+
 import "./NavMenu.css";
 
 class NavMenu extends Component {
@@ -66,9 +66,24 @@ class NavMenu extends Component {
   render() {
     return (
       <nav className="Header">
-        <Link to="/" className="Header__link">
+        <Link to="/feed" className="Header__link">
           <h2 className="NavMenu__Header">Frenmo</h2>
         </Link>
+        <div>
+          <Link to="/frenmos" className="NavMenu__links">
+            My Frenmos
+          </Link>
+        </div>
+        <div>
+          <Link to="/friends" className="NavMenu__links">
+            Friends
+          </Link>
+        </div>
+        <div>
+          <Link to="/profile" className="NavMenu__links">
+            Profile
+          </Link>
+        </div>
         {TokenService.hasAuthToken()
           ? this.renderLogoutLink()
           : this.renderLoginLink()}
--- src/components/PublicFeedItem/PublicFeedItem.js
+++ src/components/PublicFeedItem/PublicFeedItem.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
+import Check from "../../images/check.png";
 import "./PublicFeedItem.css";
 export default class PublicFeedItem extends Component {
   static contextType = UserContext;
 
-  handleRedemption = () => {
+  handleRedemption = osId => {
     let { recdById, favorId, outstandingId } = this.props;
-    const { user } = this.context;
+    console.log(osId);
 
     console.log("recd", recdById, "favId", favorId, "osId", outstandingId);
   };
@@ -15,34 +17,40 @@ export default class PublicFeedItem extends Component {
     const {
       favorId,
       title,
-      description,
-      category,
+      // description,
+      // category,
       expDate,
-      publicity,
-      originalLimit,
+      // publicity,
+      // originalLimit,
       outstandingId,
-      creatorId,
+      // creatorId,
       createdByName,
-      createdByUser,
-      issuerId,
+      // createdByUser,
+      // issuerId,
       issuedByName,
-      issuedByUser,
+      // issuedByUser,
       recdById,
-      recdByName,
-      recdByUser
+      recdByName
+      // recdByUser
     } = this.props;
     return (
       <div className="PublicFeedItem__container">
-        {recdById && <p>REDEEMED (add icon)</p>}
+        {recdById && <img src={Check} alt="check icon" />}
         <div className="PublicFeedItem__favor">
-          <p>{title}</p>
-          {recdByName && <p>To: {recdByName}</p>}
-          {issuedByName && <p>From: {issuedByName}</p>}
-          <p>Created by: {createdByName}</p>
-          {/* need to install moment to format date */}
-          <p>Redeem by: {expDate}</p>
+          <Link to={`/frenmos/${favorId}`}>
+            <p>Favor: {title}</p>
+            {recdByName && <p>To: {recdByName}</p>}
+            {issuedByName && <p>From: {issuedByName}</p>}
+            <p>Created by: {createdByName}</p>
+            {/* need to install moment to format date */}
+            <p>Redeem by: {expDate}</p>
+          </Link>
         </div>
-        {!recdById && <button onClick={this.handleRedemption}>Redeem</button>}
+        {!recdById && (
+          <button onClick={() => this.handleRedemption(outstandingId)}>
+            Redeem
+          </button>
+        )}
       </div>
     );
   }
