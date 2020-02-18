a23a21d2077328128ec344f8c05cc11a73a74b58
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-17 19:23:42 -0800

    switched rendering of logout and login buttons and fixed sizing and placement of footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import LoginPage from "./routes/LoginPage/LoginPage";
-import RegistrationPage from "./routes/RegistrationPage/RegistrationPage";
-import FeedPage from "./routes/FeedPage/FeedPage";
-import NewFrenmoPage from "./routes/NewFrenmoPage/NewFrenmoPage";
-import NavMenu from "./components/Header/NavMenu";
-import PrivateRoute from "./components/Utils/PrivateRoute";
-import PublicOnlyRoute from "./components/Utils/PublicOnlyRoute";
-import FooterMenu from "./components/FooterMenu/FooterMenu";
-import "./App.css";
-import FrenmoCategoryNavPage from "./routes/FrenmoCategoryNavPage/FrenmoCategoryNavPage";
-import FrenmoListByCat from "./routes/FrenmoListByCat/FrenmoListByCat";
-import EditFrenmoPage from "./routes/EditFrenmoPage/EditFrenmoPage";
-import FriendsList from "./components/FriendsList/FriendsList";
-import FrenmoDashboard from "./routes/FrenmoDashboard/FrenmoDashboard";
+import React, {
+  Component
+} from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
+import LoginPage from './routes/LoginPage/LoginPage';
+import RegistrationPage from './routes/RegistrationPage/RegistrationPage';
+import FeedPage from './routes/FeedPage/FeedPage';
+import NewFrenmoPage from './routes/NewFrenmoPage/NewFrenmoPage';
+import NavMenu from './components/Header/NavMenu';
+import PrivateRoute from './components/Utils/PrivateRoute';
+import PublicOnlyRoute from './components/Utils/PublicOnlyRoute';
+import FooterMenu from './components/FooterMenu/FooterMenu';
+import './App.css';
+import FrenmoCategoryNavPage from './routes/FrenmoCategoryNavPage/FrenmoCategoryNavPage';
+import FrenmoListByCat from './routes/FrenmoListByCat/FrenmoListByCat';
+import EditFrenmoPage from './routes/EditFrenmoPage/EditFrenmoPage';
+import FriendsList from './components/FriendsList/FriendsList';
+import FrenmoDashboard from './routes/FrenmoDashboard/FrenmoDashboard';
 
 class App extends Component {
   state = { hasError: false };
@@ -22,7 +27,12 @@ class App extends Component {
     return (
       <>
         <div className="Nav-flex">
-          <Route path={"/frenmos/:categoryId"} component={FrenmoListByCat} />
+          <Route
+            path={
+              '/frenmos/:categoryId'
+            }
+            component={FrenmoListByCat}
+          />
           {/* <Route
             path={"/frenmos/:categoryId"}
             component={FrenmoCategoryNavPage}
@@ -36,20 +46,39 @@ class App extends Component {
     return (
       <>
         <Switch>
-          <PublicOnlyRoute exact path={"/login"} component={LoginPage} />
+          <PublicOnlyRoute
+            exact
+            path={'/login'}
+            component={LoginPage}
+          />
 
           <PublicOnlyRoute
             exact
-            path={"/register"}
+            path={'/register'}
             component={RegistrationPage}
           />
 
-          <PublicOnlyRoute exact path={"/friends"} component={FriendsList} />
+          <PublicOnlyRoute
+            exact
+            path={'/friends'}
+            component={FriendsList}
+          />
 
-          <Route path={"/feed"} component={FeedPage} />
+          <Route
+            path={'/feed'}
+            component={FeedPage}
+          />
 
-          <PrivateRoute exact path={"/send"} component={NewFrenmoPage} />
-          <PrivateRoute exact path={`/frenmos`} component={FrenmoDashboard} />
+          <PrivateRoute
+            exact
+            path={'/send'}
+            component={NewFrenmoPage}
+          />
+          <PrivateRoute
+            exact
+            path={`/frenmos`}
+            component={FrenmoDashboard}
+          />
 
           <PrivateRoute
             exact
@@ -67,20 +96,24 @@ class App extends Component {
         <NavMenu />
         <main className="App__container wrapper">
           {this.state.hasError && (
-            <p className="red">Something went wrong. Please try again later.</p>
+            <p className="red">
+              Something went wrong.
+              Please try again later.
+            </p>
           )}
 
           <div className="Nav-flex">
-            <nav className="App__nav">{this.renderNavRoutes()}</nav>
+            <nav className="App__nav">
+              {this.renderNavRoutes()}
+            </nav>
           </div>
           <section className="App__main wrapper">
             {this.renderMainRoutes()}
           </section>
-
-          <footer id="footer">
-            <FooterMenu />
-          </footer>
         </main>
+        <footer id="footer">
+          <FooterMenu />
+        </footer>
       </div>
     );
   }
