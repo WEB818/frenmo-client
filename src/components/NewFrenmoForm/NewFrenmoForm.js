c47f3b1edc5210d6d9a1a7100cfdb096ca640251
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-19 09:28:38 -0800

    refactor ask portion to dry up the form

--- src/components/NewFrenmoForm/NewFrenmoForm.js
+++ src/components/NewFrenmoForm/NewFrenmoForm.js
@@ -106,339 +106,203 @@ class NewFrenmoForm extends Component {
   };
 
   renderForm = () => {
+    let sendPortion = (
+      <>
+        <div className="NewFrenmo__input-container">
+          <Label htmlFor="NewFrenmo__receiver">
+            Give To:
+          </Label>
+          <Input
+            type="text"
+            name="receiver"
+            id="NewFrenmo__receiver"
+            aria-label="Add receiver for frenmo"
+          />
+        </div>
+        <Button type="submit">
+          Send Frenmo
+        </Button>
+      </>
+    );
+    let askPortion = (
+      <>
+        <div className="NewFrenmo__input-container">
+          <Label htmlFor="NewFrenmo__user">
+            Request From:
+          </Label>
+          <Input
+            type="text"
+            name="user"
+            id="NewFrenmo__user"
+            aria-label="Add user for frenmo"
+          />
+        </div>
+        <Button type="submit">
+          Request Frenmo
+        </Button>
+      </>
+    );
+    let formtype;
+    let giverOrReceiver;
     if (this.state.give) {
-      return (
-        <form
-          className="NewFrenmoForm__send"
-          onSubmit={this.handleSubmit}
-        >
-          {/* <Label htmlFor="NewFrenmo__title">Redeemable For:</Label> */}
-          <Input
-            type="text"
-            name="title"
-            id="NewFrenmo__title"
-            placeholder="Redeemable for..."
-            aria-label="Add title for frenmo"
-            required
-          />
-
-          {/* <Label htmlFor="NewFrenmo__description">Frenmo Description:</Label> */}
-          <Textarea
-            id="NewFrenmo__description"
-            name="description"
-            placeholder="Add a message..."
-            aria-label="Add description for frenmo"
-            required
-          />
-
-          {/* <Label htmlFor="NewFrenmo__category">Select a category:</Label> */}
-          <select
-            id="NewFrenmo__category"
-            name="category"
-            aria-label="Select category for frenmo"
-            required
-          >
-            {/** TODO:can generate this on the fly from categories */}
-            <option value="0">
-              --Please choose a
-              category--
-            </option>
-            <option value="1">
-              Advice
-            </option>
-            <option value="2">
-              Career
-            </option>
-            <option value="3">
-              Community
-            </option>
-            <option value="4">
-              Creative
-            </option>
-            <option value="5">
-              Education
-            </option>
-            <option value="6">
-              Emergency
-            </option>
-            <option value="7">
-              Family
-            </option>
-            <option value="8">
-              Food
-            </option>
-            <option value="9">
-              Gaming
-            </option>
-            <option value="10">
-              Health
-            </option>
-            <option value="11">
-              IT
-            </option>
-            <option value="12">
-              Kids
-            </option>
-            <option value="13">
-              Miscellaneous
-            </option>
-            <option value="14">
-              Needs fixing
-            </option>
-            <option value="15">
-              Pets
-            </option>
-            <option value="16">
-              Plants
-            </option>
-            <option value="17">
-              Relationship
-            </option>
-            <option value="18">
-              Religion & Spirituality
-            </option>
-            <option value="19">
-              Ridesharing
-            </option>
-            <option value="20">
-              Sports
-            </option>
-            <option value="21">
-              Travel
-            </option>
-            <option value="22">
-              Volunteers Needed
-            </option>
-            <option value="23">
-              Wedding
-            </option>
-          </select>
-          <div className="NewFrenmo__input-container">
-            <Label htmlFor="NewFrenmo__expiration-date">
-              Valid until:
-            </Label>
-            <DatePicker
-              selected={
-                this.state.expDate
-              }
-              onChange={
-                this.handleChange
-              }
-              id="NewFrenmo__expiration-date"
-              name="expiration_date"
-              aria-label="Select expiration date for frenmo"
-            />
-          </div>
-          <select
-            id="NewFrenmo__publicity"
-            name="publicity"
-            aria-label="Select privacy setting for frenmo"
-            required
-          >
-            <option value="0">
-              --Please set privacy--
-            </option>
-            <option value="dm">
-              Direct Message
-            </option>
-            <option value="friend">
-              Friends Only
-            </option>
-            <option value="public">
-              Public
-            </option>
-          </select>
-          <div className="NewFrenmo__input-container">
-            <Label htmlFor="NewFrenmo__limit">
-              Set Limit:
-            </Label>
-            <Input
-              type="number"
-              name="limit"
-              id="NewFrenmo__limit"
-              min="1"
-              aria-label="Add limit for frenmo"
-            />
-          </div>
-          <div className="NewFrenmo__input-container">
-            <Label htmlFor="NewFrenmo__receiver">
-              Give To:
-            </Label>
-            <Input
-              type="text"
-              name="receiver"
-              id="NewFrenmo__receiver"
-              aria-label="Add receiver for frenmo"
-            />
-          </div>
-          {/**one for submitting and issuing to a person */}
-          <Button type="submit">
-            Send Frenmo
-          </Button>
-        </form>
-      );
+      formtype = 'send';
+      giverOrReceiver = sendPortion;
     } else if (this.state.ask) {
-      return (
-        <form
-          className="NewFrenmoForm__ask"
-          onSubmit={this.handleSubmit}
-        >
-          {/* <Label htmlFor="NewFrenmo__title">Redeemable For:</Label> */}
-          <Input
-            type="text"
-            name="title"
-            id="NewFrenmo__title"
-            placeholder="Redeemable for..."
-            aria-label="Add title for frenmo"
-            required
-          />
-
-          {/* <Label htmlFor="NewFrenmo__description">Frenmo Description:</Label> */}
-          <Textarea
-            id="NewFrenmo__description"
-            name="description"
-            placeholder="Add a message..."
-            aria-label="Add description for frenmo"
-            required
-          />
-
-          {/* <Label htmlFor="NewFrenmo__category">Select a category:</Label> */}
-          <select
-            id="NewFrenmo__category"
-            name="category"
-            aria-label="Select category for frenmo"
-            required
-          >
-            {/** TODO:can generate this on the fly from categories */}
-            <option value="0">
-              --Please choose a
-              category--
-            </option>
-            <option value="1">
-              Advice
-            </option>
-            <option value="2">
-              Career
-            </option>
-            <option value="3">
-              Community
-            </option>
-            <option value="4">
-              Creative
-            </option>
-            <option value="5">
-              Education
-            </option>
-            <option value="6">
-              Emergency
-            </option>
-            <option value="7">
-              Family
-            </option>
-            <option value="8">
-              Food
-            </option>
-            <option value="9">
-              Gaming
-            </option>
-            <option value="10">
-              Health
-            </option>
-            <option value="11">
-              IT
-            </option>
-            <option value="12">
-              Kids
-            </option>
-            <option value="13">
-              Miscellaneous
-            </option>
-            <option value="14">
-              Needs fixing
-            </option>
-            <option value="15">
-              Pets
-            </option>
-            <option value="16">
-              Plants
-            </option>
-            <option value="17">
-              Relationship
-            </option>
-            <option value="18">
-              Religion & Spirituality
-            </option>
-            <option value="19">
-              Ridesharing
-            </option>
-            <option value="20">
-              Sports
-            </option>
-            <option value="21">
-              Travel
-            </option>
-            <option value="22">
-              Volunteers Needed
-            </option>
-            <option value="23">
-              Wedding
-            </option>
-          </select>
-          <div className="NewFrenmo__input-container">
-            <Label htmlFor="NewFrenmo__expiration-date">
-              Valid until:
-            </Label>
-            <DatePicker
-              selected={
-                this.state.expDate
-              }
-              onChange={
-                this.handleChange
-              }
-              id="NewFrenmo__expiration-date"
-              name="expiration_date"
-              aria-label="Select expiration date for frenmo"
-            />
-          </div>
-          <select
-            id="NewFrenmo__publicity"
-            name="publicity"
-            aria-label="Select privacy setting for frenmo"
-            required
-          >
-            <option value="0">
-              --Please set privacy--
-            </option>
-            <option value="dm">
-              Direct Message
-            </option>
-            <option value="friend">
-              Friends Only
-            </option>
-            <option value="public">
-              Public
-            </option>
-          </select>
-          <div className="NewFrenmo__input-container">
-            <Label htmlFor="NewFrenmo__limit">
-              Set Limit:
-            </Label>
-            <Input
-              type="number"
-              name="limit"
-              id="NewFrenmo__limit"
-              min="1"
-              aria-label="Add limit for frenmo"
-            />
-          </div>
-
-          {/**one for submitting and issuing to yourself */}
-          <Button type="submit">
-            Request Frenmo
-          </Button>
-        </form>
-      );
+      formtype = 'ask';
+      giverOrReceiver = askPortion;
     }
+    let generalForm = (
+      <form
+        className={`NewFrenmoForm__${formtype}`}
+        onSubmit={this.handleSubmit}
+      >
+        {/* <Label htmlFor="NewFrenmo__title">Redeemable For:</Label> */}
+        <Input
+          type="text"
+          name="title"
+          id="NewFrenmo__title"
+          placeholder="Redeemable for..."
+          aria-label="Add title for frenmo"
+          required
+        />
+
+        {/* <Label htmlFor="NewFrenmo__description">Frenmo Description:</Label> */}
+        <Textarea
+          id="NewFrenmo__description"
+          name="description"
+          placeholder="Add a message..."
+          aria-label="Add description for frenmo"
+          required
+        />
+
+        {/* <Label htmlFor="NewFrenmo__category">Select a category:</Label> */}
+        <select
+          id="NewFrenmo__category"
+          name="category"
+          aria-label="Select category for frenmo"
+          required
+        >
+          {/** TODO:can generate this on the fly from categories */}
+          <option value="0">
+            --Please choose a category--
+          </option>
+          <option value="1">
+            Advice
+          </option>
+          <option value="2">
+            Career
+          </option>
+          <option value="3">
+            Community
+          </option>
+          <option value="4">
+            Creative
+          </option>
+          <option value="5">
+            Education
+          </option>
+          <option value="6">
+            Emergency
+          </option>
+          <option value="7">
+            Family
+          </option>
+          <option value="8">
+            Food
+          </option>
+          <option value="9">
+            Gaming
+          </option>
+          <option value="10">
+            Health
+          </option>
+          <option value="11">IT</option>
+          <option value="12">
+            Kids
+          </option>
+          <option value="13">
+            Miscellaneous
+          </option>
+          <option value="14">
+            Needs fixing
+          </option>
+          <option value="15">
+            Pets
+          </option>
+          <option value="16">
+            Plants
+          </option>
+          <option value="17">
+            Relationship
+          </option>
+          <option value="18">
+            Religion & Spirituality
+          </option>
+          <option value="19">
+            Ridesharing
+          </option>
+          <option value="20">
+            Sports
+          </option>
+          <option value="21">
+            Travel
+          </option>
+          <option value="22">
+            Volunteers Needed
+          </option>
+          <option value="23">
+            Wedding
+          </option>
+        </select>
+        <div className="NewFrenmo__input-container">
+          <Label htmlFor="NewFrenmo__expiration-date">
+            Valid until:
+          </Label>
+          <DatePicker
+            selected={
+              this.state.expDate
+            }
+            onChange={this.handleChange}
+            id="NewFrenmo__expiration-date"
+            name="expiration_date"
+            aria-label="Select expiration date for frenmo"
+          />
+        </div>
+        <select
+          id="NewFrenmo__publicity"
+          name="publicity"
+          aria-label="Select privacy setting for frenmo"
+          required
+        >
+          <option value="0">
+            --Please set privacy--
+          </option>
+          <option value="dm">
+            Direct Message
+          </option>
+          <option value="friend">
+            Friends Only
+          </option>
+          <option value="public">
+            Public
+          </option>
+        </select>
+        <div className="NewFrenmo__input-container">
+          <Label htmlFor="NewFrenmo__limit">
+            Set Limit:
+          </Label>
+          <Input
+            type="number"
+            name="limit"
+            id="NewFrenmo__limit"
+            min="1"
+            aria-label="Add limit for frenmo"
+          />
+        </div>
+        {giverOrReceiver}
+      </form>
+    );
+    return generalForm;
   };
 
   render() {
