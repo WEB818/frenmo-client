6577a5cb27fd1d07603f71a1aced77850c26be1f
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-27 14:30:49 -0800

    made a splash page and base styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import LoginPage from "./routes/LoginPage/LoginPage";
-import RegistrationPage from "./routes/RegistrationPage/RegistrationPage";
-import FeedPage from "./routes/FeedPage/FeedPage";
-import NewFrenmoPage from "./routes/NewFrenmoPage/NewFrenmoPage";
-import NavMenu from "./components/Header/NavMenu";
-import PrivateRoute from "./components/Utils/PrivateRoute";
-import PublicOnlyRoute from "./components/Utils/PublicOnlyRoute";
-import FooterMenu from "./components/FooterMenu/FooterMenu";
-import FrenmoContext from "./contexts/FrenmoContext";
-import FrenmoListByCat from "./routes/FrenmoListByCat/FrenmoListByCat";
-import EditFrenmoPage from "./routes/EditFrenmoPage/EditFrenmoPage";
-import Friends from "./routes/Friends/Friends";
-import PendingFren from "./components/PendingFren/PendingFren";
-import FrenmoDashboard from "./routes/FrenmoDashboard/FrenmoDashboard";
-import FrenmoDetail from "./components/FrenmoDetail/FrenmoDetail";
+import React, {
+  Component
+} from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
+import LoginPage from './routes/LoginPage/LoginPage';
+import RegistrationPage from './routes/RegistrationPage/RegistrationPage';
+import FeedPage from './routes/FeedPage/FeedPage';
+import NewFrenmoPage from './routes/NewFrenmoPage/NewFrenmoPage';
+import NavMenu from './components/Header/NavMenu';
+import PrivateRoute from './components/Utils/PrivateRoute';
+import PublicOnlyRoute from './components/Utils/PublicOnlyRoute';
+import FooterMenu from './components/FooterMenu/FooterMenu';
+import FrenmoContext from './contexts/FrenmoContext';
+import FrenmoListByCat from './routes/FrenmoListByCat/FrenmoListByCat';
+import EditFrenmoPage from './routes/EditFrenmoPage/EditFrenmoPage';
+import Friends from './routes/Friends/Friends';
+import PendingFren from './components/PendingFren/PendingFren';
+import FrenmoDashboard from './routes/FrenmoDashboard/FrenmoDashboard';
+import FrenmoDetail from './components/FrenmoDetail/FrenmoDetail';
 
-import Frenmo from "./components/Frenmo/Frenmo";
-import { PopupFeedback } from "./components/PopupFeedback";
-import NewFrenmoForm from "./components/NewFrenmoForm/NewFrenmoForm";
-import "./App.scss";
+import Frenmo from './components/Frenmo/Frenmo';
+import { PopupFeedback } from './components/PopupFeedback';
+import NewFrenmoForm from './components/NewFrenmoForm/NewFrenmoForm';
+import './App.scss';
+import Splash from './components/Splash/Splash';
 
 class App extends Component {
   state = {
@@ -31,26 +37,60 @@ class App extends Component {
   static contextType = FrenmoContext;
 
   renderNavRoutes() {
-    return <PrivateRoute path={`/frenmos`} component={FrenmoDashboard} />;
+    return (
+      <PrivateRoute
+        path={`/frenmos`}
+        component={FrenmoDashboard}
+      />
+    );
   }
 
   renderMainRoutes() {
     return (
       <>
         <Switch>
-          <PublicOnlyRoute path={"/login"} component={LoginPage} />
-          <PublicOnlyRoute path={"/register"} component={RegistrationPage} />
-          <PrivateRoute path={"/pending"} component={PendingFren} />
-          <PrivateRoute exact path={"/friends"} component={Friends} />
-          <PrivateRoute path={"/feed"} component={FeedPage} />
-          <PrivateRoute exact path={"/send"} component={NewFrenmoPage} />
+          <PublicOnlyRoute
+            exact
+            path={'/'}
+            component={Splash}
+          ></PublicOnlyRoute>
+          <PublicOnlyRoute
+            path={'/login'}
+            component={LoginPage}
+          />
+          <PublicOnlyRoute
+            path={'/register'}
+            component={RegistrationPage}
+          />
           <PrivateRoute
-            path={"/frenmos/category/:categoryId"}
+            path={'/pending'}
+            component={PendingFren}
+          />
+          <PrivateRoute
+            exact
+            path={'/friends'}
+            component={Friends}
+          />
+          <PrivateRoute
+            path={'/feed'}
+            component={FeedPage}
+          />
+          <PrivateRoute
+            exact
+            path={'/send'}
+            component={NewFrenmoPage}
+          />
+          <PrivateRoute
+            path={
+              '/frenmos/category/:categoryId'
+            }
             component={FrenmoListByCat}
           />
           <PrivateRoute
             exact
-            path={"/frenmos/:outstandingId/edit"}
+            path={
+              '/frenmos/:outstandingId/edit'
+            }
             component={EditFrenmoPage}
           />
         </Switch>
@@ -63,7 +103,9 @@ class App extends Component {
       <>
         <Switch>
           <PrivateRoute
-            path={"/frenmos/category/:categoryId/:outstandingId"}
+            path={
+              '/frenmos/category/:categoryId/:outstandingId'
+            }
             component={FrenmoDetail}
           />
         </Switch>
@@ -89,14 +131,21 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.feedbackMessage ? (
-          <PopupFeedback feedbackMessage={this.state.feedbackMessage} />
+          <PopupFeedback
+            feedbackMessage={
+              this.state.feedbackMessage
+            }
+          />
         ) : null}
         <NavMenu />
 
         <main className="App__container wrapper">
           {this.renderNavRoutes()}
           {this.state.hasError && (
-            <p className="red">Something went wrong. Please try again later.</p>
+            <p className="red">
+              Something went wrong.
+              Please try again later.
+            </p>
           )}
 
           <section className="App__main wrapper">
