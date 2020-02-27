f77d5ce4bfd463925dbc0bc68b9a1f66f4da16a1
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-27 08:21:29 -0800

    removed console logs from FrenmoTags

--- src/components/FrenmoTags/FrenmoTags.js
+++ src/components/FrenmoTags/FrenmoTags.js
@@ -1,20 +1,28 @@
-import React from "react";
-import "./FrenmoTags.css";
+import React from 'react';
+import './FrenmoTags.css';
 
 function FrenmoTags() {
-  const [tags, setTags] = React.useState(["Im a tag"]);
+  const [
+    tags,
+    setTags
+  ] = React.useState(['Im a tag']);
   const addTags = event => {
-    if (event.key === "Enter") {
-      setTags([...tags, event.target.value]);
-      event.target.value = "";
+    if (event.key === 'Enter') {
+      setTags([
+        ...tags,
+        event.target.value
+      ]);
+      event.target.value = '';
     }
   };
-  console.log(tags);
   return (
     <div className="tags-input">
       <ul>
         {tags.map((tag, index) => (
-          <li key={index} className="FrenmoTags__tag">
+          <li
+            key={index}
+            className="FrenmoTags__tag"
+          >
             <span>
               {tag}
               <p>X</p>
