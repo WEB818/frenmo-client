06c66e74c862f16d758c404dd9eacbff19fe6b69
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-27 08:27:36 -0800

    removed console logs from IssueFrenmo, NewFrenmoForm, PendingFren, SearchBar and others,

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,45 +1,68 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import "../../components/FriendsList/FriendsList.css"
+import React, {
+  Component
+} from 'react';
+import { Link } from 'react-router-dom';
+import '../../components/FriendsList/FriendsList.css';
 
 export default class FriendsList extends Component {
   state = {
-    sendFrenmo: false,
-  }
+    sendFrenmo: false
+  };
 
-  
-    unfriendById = id => {
-      this.props.update(id)
-    }
-    
-  
-  
+  unfriendById = id => {
+    this.props.update(id);
+  };
+
   frenmo = id => {
-    console.log('click',id)
-    if(!this.state.sendFrenmo){
-      alert('your frenmo form')
+    if (!this.state.sendFrenmo) {
+      alert('your frenmo form');
       this.setState({
-        sendFrenmo: true,
-      })
+        sendFrenmo: true
+      });
     }
 
     this.setState({
       sendFrenmo: false
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div>
         <ul className="friends-list">
-           <li onDoubleClick={() => this.frenmo(this.props.frens.id)} className="frens" key={this.props.frens.id}>
-             {this.props.frens.username} 
-             <button type="submit" onClick={() => this.frenmo(this.props.frens.id)}>Send Frenmo</button>
-             <button type="submit" onClick={() => this.unfriendById(this.props.frens.id)}>unfriend</button>
-             </li>
+          <li
+            onDoubleClick={() =>
+              this.frenmo(
+                this.props.frens.id
+              )
+            }
+            className="frens"
+            key={this.props.frens.id}
+          >
+            {this.props.frens.username}
+            <button
+              type="submit"
+              onClick={() =>
+                this.frenmo(
+                  this.props.frens.id
+                )
+              }
+            >
+              Send Frenmo
+            </button>
+            <button
+              type="submit"
+              onClick={() =>
+                this.unfriendById(
+                  this.props.frens.id
+                )
+              }
+            >
+              unfriend
+            </button>
+          </li>
         </ul>
       </div>
-    )
+    );
   }
 }
-
--- src/components/PendingFren/PendingFren.js
+++ src/components/PendingFren/PendingFren.js
@@ -1,59 +1,83 @@
-import React, { Component } from 'react'
-import config from '../../config'
-import { Link } from 'react-router-dom'
+import React, {
+  Component
+} from 'react';
+import config from '../../config';
+import { Link } from 'react-router-dom';
 
-import PendingFriends from '../../routes/PendingFriends/PendingFriends'
-import TokenService from '../../services/token-service'
+import PendingFriends from '../../routes/PendingFriends/PendingFriends';
+import TokenService from '../../services/token-service';
 
 export class PendingFren extends Component {
-    state = {
-      pendingFrens: []
-    }
+  state = {
+    pendingFrens: []
+  };
 
-     setPendingFrens = pending => {
-         console.log("pending",pending)
-        this.setState({
-            pendingFrens: pending,
-        })
-    }
-    componentDidMount(){
-        fetch(`${config.API_ENDPOINT}/friend/pending`, {
-            method: 'GET',
-            headers: {
-                authorization : `bearer ${TokenService.getAuthToken()}`
-            }
-        })
-        .then(res => {
-            return res.json()
-        })
-        .then(this.setPendingFrens)
-    }
+  setPendingFrens = pending => {
+    this.setState({
+      pendingFrens: pending
+    });
+  };
+  componentDidMount() {
+    fetch(
+      `${config.API_ENDPOINT}/friend/pending`,
+      {
+        method: 'GET',
+        headers: {
+          authorization: `bearer ${TokenService.getAuthToken()}`
+        }
+      }
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(this.setPendingFrens);
+  }
 
-    updateFriendsAfterAdd = frensId => {
-        fetch(`${config.API_ENDPOINT}/friend/${frensId}`, {
-            method: 'PATCH',
-            headers: {
-                authorization : `bearer ${TokenService.getAuthToken()}`
-            }
-        })
-        .then(res => this.setState({
-            pendingFrens: [...this.state.pendingFrens.filter(fren => fren.id !== frensId)]
-        }))
-    }
+  updateFriendsAfterAdd = frensId => {
+    fetch(
+      `${config.API_ENDPOINT}/friend/${frensId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          authorization: `bearer ${TokenService.getAuthToken()}`
+        }
+      }
+    ).then(res =>
+      this.setState({
+        pendingFrens: [
+          ...this.state.pendingFrens.filter(
+            fren => fren.id !== frensId
+          )
+        ]
+      })
+    );
+  };
 
-
-    render() {
-        console.log('state',this.state.pendingFrens)
-        return (
-            <div>
-                <div>
-                <h2>pending friends</h2>
-                <span><Link to='/Friends'>Friends List</Link></span>
-                </div>
-                {this.state.pendingFrens.map(pen => <PendingFriends pending={pen} update={this.updateFriendsAfterAdd}/>)}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>pending friends</h2>
+          <span>
+            <Link to="/Friends">
+              Friends List
+            </Link>
+          </span>
+        </div>
+        {this.state.pendingFrens.map(
+          pen => (
+            <PendingFriends
+              pending={pen}
+              update={
+                this
+                  .updateFriendsAfterAdd
+              }
+            />
+          )
+        )}
+      </div>
+    );
+  }
 }
 
-export default PendingFren
+export default PendingFren;
