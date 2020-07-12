510e718b1e962e4b607d7966bde329ed7642ebf0
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-07-12 14:00:23 -0700

    fix tests

--- src/components/FriendsList/FriendsList.test.js
+++ src/components/FriendsList/FriendsList.test.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import FriendsList from "./FriendsList";
 
 describe(`FriendsList Component`, () => {
   it("renders without errors", () => {
-    const friend = {id: 1}
+    const friend = { id: 1 };
     const div = document.createElement("div");
-    ReactDOM.render(<FriendsList friend={friend} />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <FriendsList friend={friend} />
+      </BrowserRouter>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
